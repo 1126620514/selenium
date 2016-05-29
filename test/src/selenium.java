d9c13083e31a3d011fd2025549ee194c90a77a1b
1126620514 <1126620514@qq.com>
2016-05-29 17:04:34 +0800

    啊啊

---
+++ test/src/selenium.java
@@ -0,0 +1,16 @@
+
+
+public class selenium {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		System.setProperty("webdriver.firfox.bin/", "F:/Mozilla Firefox/firefox.exe");
+//		WebDriver driver=new FirefoxDriver();
+//		driver.get("www.baidu.com");
+		System.out.println("this is test selenium");
+	}
+
+}
